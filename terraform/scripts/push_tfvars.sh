5d5ec54dc0b1f044478c361428bfffbd45d3c76d
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-11-02 22:29:31 -0400

    Modify the tfvars scripts to handle multiple files

---
+++ terraform/scripts/push_tfvars.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Usage:
+#   terraform/push_tfvars.sh <tfvars_filename_no_extension>
+
+# Push your local version of $1.tfvars to the correct S3 bucket so
+# that it can be used by others.
+#
+# Note that this script requires that the AWS command line interface
+# be installed on your system.
+
+TERRAFORM_TFVARS_S3_BUCKET="ncats-terraform-production-tfvars"
+TERRAFORM_DIR="terraform-pca-teamserver"
+TFVARS_FILE="$1.tfvars"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+aws s3 cp ${SCRIPT_DIR}/../${TFVARS_FILE} s3://${TERRAFORM_TFVARS_S3_BUCKET}/${TERRAFORM_DIR}/${TFVARS_FILE}
