90ac74b9a19a0a735b090826c63577d5613a21dd
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-30 13:54:30 -0400

    Add a new provider that is able to make changes to DNS entries in the COOL cyber.dhs.gov zone.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,3 +4,10 @@ provider "aws" {
   region = var.aws_region
 }
 
+# This is the provider that can make changes to DNS entries
+# in the cyber.dhs.gov zone.
+provider "aws" {
+  alias  = "dns"
+  profile = "cool-dns-route53resourcechange-cyber.dhs.gov"
+  region = var.aws_region  # route53 is global, but still required by Terraform
+}
