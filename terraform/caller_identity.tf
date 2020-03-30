ed76e9df42292c6868525aa8643714402361072c
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-30 13:53:50 -0400

    Move provider and caller identity to their own files.

--- terraform/caller_identity.tf
+++ terraform/caller_identity.tf
@@ -1,8 +1,3 @@
-# Configure AWS
-provider "aws" {
-  region = var.aws_region
-}
-
 # The AWS account ID being used
 data "aws_caller_identity" "current" {
 }
---
+++ terraform/providers.tf
@@ -0,0 +1,6 @@
+# This is the default Terraform provider that is used if no
+# other provider is specified.
+provider "aws" {
+  region = var.aws_region
+}
+
