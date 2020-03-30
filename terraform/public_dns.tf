214fd4aa84b2b0dc90bf035167e50932c1002207
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-30 13:55:13 -0400

    Use the new DNS provider when creating the public A record for the teamserver.

--- terraform/public_dns.tf
+++ terraform/public_dns.tf
@@ -3,6 +3,8 @@ data "aws_route53_zone" "public_zone" {
 }
 
 resource "aws_route53_record" "teamserver_pub_A" {
+  provider = aws.dns
+  
   zone_id = data.aws_route53_zone.public_zone.zone_id
   name    = "teamserver.${terraform.workspace}.${local.public_subdomain}${data.aws_route53_zone.public_zone.name}"
   type    = "A"
