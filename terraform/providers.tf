7511a5608d7c4a660a9dabd86e4c49fdee2909a5
Dave Redmin <david.redmin@beta.dhs.gov>
2020-03-31 10:15:12 -0400

    Add a comment explaining why we can't currently assume the DNS role via Terraform remote state.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,11 @@ provider "aws" {
 
 # This is the provider that can make changes to DNS entries
 # in the cyber.dhs.gov zone.
+#
+# NOTE: After we begin deploying these TeamServers in the COOL, it
+# will be possible to assume the role below via Terraform remote
+# state.  For details see:
+# https://github.com/cisagov/pca-teamserver-aws/pull/30#discussion_r400610194
 provider "aws" {
   alias   = "dns"
   profile = "cool-dns-route53resourcechange-cyber.dhs.gov"
